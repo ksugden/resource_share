10df2be55cb9f50cdb5780b31e27f8b0af996378
Kate Sugden <kate.sugden@madetech.com>
2022-06-30 12:39:49 +0100

    Add FilterExpressions to queries

--- backend/utilities.py
+++ backend/utilities.py
@@ -1,18 +1,17 @@
 from datetime import datetime
 import boto3
-from boto3.dynamodb.conditions import Key
+from boto3.dynamodb.conditions import Key, Attr
 
 
-USERS_TABLE = 'Users'
-RESOURCES_TABLE = 'Resources'
-BOOKINGS_TABLE = 'Bookings'
+USERS_TABLE = 'Users' # PK = user_id
+RESOURCES_TABLE = 'Resources' # PK = resource_id
+BOOKINGS_TABLE = 'Bookings' # PK = resource_id and start
 
 
 TIMESLOT_HOUR_GRANULARITY = 1
 ADVANCE_BOOK_WEEKS_LIMIT = 1
 
 
-dynamodb_client = boto3.client('dynamodb', region_name="eu-west-1")
 dynamodb = boto3.resource('dynamodb', region_name="eu_west-1")
 
 
@@ -22,25 +21,26 @@ bookings_table = dynamodb.Table(BOOKINGS_TABLE)
 
 
 def get_upcoming_bookings(user_id, days):
-  now = datetime.now()
-  period_end = now + datetime.timedelta(days=days)
-  response = bookings_table.query(
-    KeyConditionExpression=Key('user_id').eq(user_id) & Key('start').between(now, period_end)
-  )
-  return response['Items']
+    now = datetime.now()
+    period_end = now + datetime.timedelta(days=days)
+    response = bookings_table.query(
+        FilterExpression = Attr('user_id').eq(user_id) & Attr('start').between(now, period_end)
+    )
+    return response['Items']
 
 
 def resource_bookings(resource_id, start, finish):
-  response = bookings_table.query(
-    KeyConditionExpression=Key('resource_id').eq(resource_id) & Key('start').lte(finish) & Key('finish').gte(start)
-  )
-  return response['Items']
+    response = bookings_table.query(
+        KeyConditionExpression = Key('resource_id').eq(resource_id) & Key('start').lt(finish),
+        FilterExpression = Attr('finish').gt(start)
+    )
+    return response['Items']
 
 
 def is_resource_available(resource_id, start, finish):
-  number_of_bookings = resource_bookings(resource_id, start, finish).count
-  return number_of_bookings == 0
+    number_of_bookings = len(resource_bookings(resource_id, start, finish))
+    return number_of_bookings == 0
 
 
 def add_booking(user_id, resource_id, start, finish):
-  pass
+    pass
