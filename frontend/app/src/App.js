458dad41473aa83e0a5f63206b94b68f8445b888
Kate Sugden <kate.sugden@madetech.com>
2022-07-22 14:09:31 +0100

    Propagate resource id to timeslot selector

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,5 +1,6 @@
 import resourceShareLogo from './resourceShareLogo.png';
 import chestnut from './chestnut.png';
+import icelandic from './icelandic.png';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import Stack from '@mui/material/Stack';
@@ -11,6 +12,15 @@ import Select, { SelectChangeEvent } from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import Divider from '@mui/material/Divider';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+
 
 const theme = createTheme({
   status: {
@@ -41,6 +51,10 @@ const timeslotsDummy = {
   'Friday 22-07': ['09:00', '12:00', '16:00', '15:00', '16:00', '17:00'],
   'Saturday 23-07': ['12:00', '13:00', '14:00']
 };
+const resourcesDummy = [
+  { 'name': 'Dobbin', 'id': 1, 'type': 'Horse' },
+  { 'name': 'Pixie', 'id': 2, 'type': 'Horse' }
+];
 
 const allowed_durations = ['1 hour', '2 hours', '3 hours', '4 hours'];
 const miliseconds_per_hour = 60 * 60 * 1000;
@@ -56,9 +70,13 @@ export default function App() {
   const [startDate, setStartDate] = useState("");
   const [startTime, setStartTime] = useState("");
   const [duration, setDuration] = useState("");
+  const [resourceId, setResourceId] = useState("");
   const [finish, setFinish] = useState("");
   const [message, setMessage] = useState("");
 
+  const sendResourceIdToParent = (resourceId) => {
+    setResourceId(resourceId);
+  }
 
   const sendDurationToParent = (hours) => {
     setDuration(hours);
@@ -125,14 +143,15 @@ export default function App() {
         <img src={resourceShareLogo} className="App-logo" alt="logo" />
         <h1>Share Hub</h1>
       </header>
-      
+
       <div>
         <article className="Book-element">
           <ThemeProvider theme={theme}>
             <div className='wrapper'>
               <Stack spacing={4}>
-              <img className='round-img' src={chestnut} id="id"></img>
+                <img className='round-img' src={chestnut} id="id"></img>
                 <TimeslotSelector
+                  resourceId={resourceId}
                   sendDateToParent={sendDateToParent}
                   sendTimeToParent={sendTimeToParent}
                   sendDurationToParent={sendDurationToParent}
@@ -143,6 +162,9 @@ export default function App() {
                   </Button>
                 </form>
                 <div className="message">{message ? <p>{message}</p> : null}</div>
+                <ResourceList 
+                  sendResourceIdToParent={sendResourceIdToParent}
+                />
               </Stack>
             </div>
           </ThemeProvider>
@@ -153,71 +175,140 @@ export default function App() {
 }
 
 
+class ResourceList extends React.Component {
+  
+  constructor(props) {
+    super(props)
+
+    this.handleResourceChange = this.handleResourceChange.bind(this);
+    this.sendResourceIdToParent = props.sendResourceIdToParent;
+
+    this.state = {
+      resources: [],
+      selectedIndex: (1)
+    }
+  }
+
+ 
+  handleResourceChange(event, index) {
+    console.log('on click event :' + index);
+    this.setState({ selectedIndex: index });
+    // this.setResourceId({  });
+    this.sendResourceIdToParent((index+1).toString());
+    // this.sendResourceImgToParent();
+  }
+
+
+  render() {
+    return (
+      <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <nav aria-label="main mailbox folders">
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton
+                selected={this.state.selectedIndex === 0}
+                onClick={(event) => this.handleResourceChange(event, 0)}
+              >
+                <ListItemAvatar>
+                  <Avatar alt='Dobbin' src={chestnut} id="id"></Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="Dobbin" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton
+                selected={this.state.selectedIndex === 1}
+                onClick={(event) => this.handleResourceChange(event, 1)}
+              >
+                <ListItemAvatar>
+                  <Avatar src={icelandic} id="id"></Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="Pixie" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </nav>
+      </Box>
+    )
+
+
+  }
+}
+
+
 
 class TimeslotSelector extends React.Component {
   constructor(props) {
     super(props)
-
+    
     this.sendDateToParent = props.sendDateToParent;
     this.sendTimeToParent = props.sendTimeToParent;
+    this.resourceId = props.resourceId;
     this.sendDurationToParent = props.sendDurationToParent;
+    
     this.handleDateChange = this.handleDateChange.bind(this);
     this.handleTimeChange = this.handleTimeChange.bind(this);
     this.handleDurationChange = this.handleDurationChange.bind(this);
-
+    
     this.state = {
       duration: '',
-      availableSlots: {},
+      availableSlots: {'No dates calculated':['no time', 'no time']},
       firstLevel: '',
       secondLevel: ''
     }
   }
-
-
-
+  
+  
+  
   handleDurationChange(event) {
     var duration = event.target.value.substring(0, 1);
+    var list_bookings_url = api_url + "list_bookings?id=" + this.props.resourceId + "&hours=";
     console.log('event value: ' + duration)
+    this.props.resourceId != '' ? 
+    console.log('fetching: ' + list_bookings_url + duration): console.log('Not fetching!');
+    
+    this.props.resourceId != '' ? 
     fetch(list_bookings_url + duration).then(
       response => response.json()
-    ).then(
-      data => {
-        this.setState({ availableSlots: data });
+      ).then(
+        data => {
+          this.setState({ availableSlots: data });
+        }
+        ) : console.log('No resource Id selected');
+        this.setState({ duration: duration });
+        this.sendDurationToParent(duration);
       }
-    );
-    this.setState({ duration: duration });
-    this.sendDurationToParent(duration);
-  }
-
-  handleDateChange(event) {
-    this.setState({ firstLevel: event.target.value });
-    this.sendDateToParent(event.target.value);
-  }
-
-  handleTimeChange(event) {
-    this.setState({ secondLevel: event.target.value });
-    this.sendTimeToParent(event.target.value);
-  }
-
-  timezone(times) {
-    var tz_times = new Array(times.length);
-    for (let t = 0; t < times.length; t++) {
-      tz_times[t] = new Date(times[t] + ' UTC');
-      tz_times[t] = tz_times[t].time();
-    }
-  }
-
-  render() {
-    const renderOption = item => <MenuItem key={item} value={item}>{item}</MenuItem>
-    const firstLevelOptions = Object.keys(this.state.availableSlots).map(renderOption)
-    console.log('this.state.availableSlots[this.state.firstLevel]: ' + this.state.availableSlots[this.state.firstLevel])
-    // console.log('this.state.availableSlots[this.state.firstLevel].length: '+this.state.availableSlots[this.state.firstLevel].length)
-    // var tz_times = this.timezone(this.state.availableSlots[this.state.firstLevel]);
-    // console.log('timzoned times: '+ tz_times);
-    const secondLevelOptions = this.state.firstLevel.length ? this.state.availableSlots[this.state.firstLevel].map(renderOption) : []
-
-
-    return (
+      
+      handleDateChange(event) {
+        this.setState({ firstLevel: event.target.value });
+        this.sendDateToParent(event.target.value);
+      }
+      
+      handleTimeChange(event) {
+        this.setState({ secondLevel: event.target.value });
+        this.sendTimeToParent(event.target.value);
+      }
+      
+      timezone(times) {
+        var tz_times = new Array(times.length);
+        for (let t = 0; t < times.length; t++) {
+          tz_times[t] = new Date(times[t] + ' UTC');
+          tz_times[t] = tz_times[t].time();
+        }
+      }
+      
+      render() {
+        console.log('resourceId:'+this.props.resourceId);
+        const renderOption = item => <MenuItem key={item} value={item}>{item}</MenuItem>
+        const firstLevelOptions = Object.keys(this.state.availableSlots).map(renderOption)
+        console.log('this.state.availableSlots[this.state.firstLevel]: ' + this.state.availableSlots[this.state.firstLevel])
+        // console.log('this.state.availableSlots[this.state.firstLevel].length: '+this.state.availableSlots[this.state.firstLevel].length)
+        // var tz_times = this.timezone(this.state.availableSlots[this.state.firstLevel]);
+        // console.log('timzoned times: '+ tz_times);
+        const secondLevelOptions = this.state.firstLevel.length ? this.state.availableSlots[this.state.firstLevel].map(renderOption) : []
+        
+        
+        return (
       <Stack spacing={2}>
 
         <div className='wrapper'>
